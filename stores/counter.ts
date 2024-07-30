3d5f5a33c45d9f58a3db2e7177197e5817bc6753
Arthur <chaff.arthur@gmail.com>
2024-07-30 15:59:18 +0200

    last files

---
+++ stores/counter.ts
@@ -0,0 +1,11 @@
+export const useCounterStore = defineStore('counter', () => {
+    const count = ref(0);
+    const increment = () => {
+        count.value++;
+    };
+
+    return {
+        count,
+        increment
+    };
+});
